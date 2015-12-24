989bb401a2173a479d27f8a68cc4e0e52793aca1
Travis Thieman <travis.thieman@gmail.com>
2015-12-24 00:40:52 -0500

    Add support for passing Redis password

--- tarrasch/database.py
+++ tarrasch/database.py
@@ -1,5 +1,6 @@
 from redis import StrictRedis
 
-from .config import REDIS_HOST, REDIS_PORT, REDIS_DB
+from .config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD
 
-singleton = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+singleton = StrictRedis(host=REDIS_HOST, port=REDIS_PORT,
+                        db=REDIS_DB, password=REDIS_PASSWORD)
