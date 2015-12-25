0c08438d2db15180b0bfdf760182d232ebe5a5c4
Travis Thieman <travis.thieman@gmail.com>
2015-12-25 16:47:33 -0500

    Okay I forgot how to program

--- tarrasch/shortener.py
+++ tarrasch/shortener.py
@@ -3,6 +3,6 @@ import urllib
 import requests
 
 def shorten_url(url):
-    r = requests.get('http://is.gd/create.php?format=simple&url={}'.format(urllib.urlencode(url)))
+    r = requests.get('http://is.gd/create.php?format=simple&url={}'.format(urllib.quote(url)))
     r.raise_for_status()
     return r.text
