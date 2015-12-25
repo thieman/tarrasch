bae17844242a4cdd1a376ea873b72d7840b8ed81
Travis Thieman <travis.thieman@gmail.com>
2015-12-24 23:07:22 -0500

    Enable Redis config from URL

--- tarrasch/config.py
+++ tarrasch/config.py
@@ -3,6 +3,7 @@ import os
 SLACK_TOKEN = os.getenv('TARRASCH_SLACK_TOKEN')
 MESSAGE_PREFIX = os.getenv('TARRASCH_MESSAGE_PREFIX', 'chess')
 
+REDIS_URL = os.getenv('TARRASCH_REDIS_URL')
 REDIS_HOST = os.getenv('TARRASCH_REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.getenv('TARRASCH_REDIS_PORT', 6379))
 REDIS_DB = int(os.getenv('TARRASCH_REDIS_DB', 0))
--- tarrasch/database.py
+++ tarrasch/database.py
@@ -1,6 +1,10 @@
 from redis import StrictRedis
+from redis.utils import from_url
 
-from .config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD
+from .config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD, REDIS_URL
 
-singleton = StrictRedis(host=REDIS_HOST, port=REDIS_PORT,
-                        db=REDIS_DB, password=REDIS_PASSWORD)
+if REDIS_URL:
+    singleton = from_url(REDIS_URL, db=REDIS_DB)
+else:
+    singleton = StrictRedis(host=REDIS_HOST, port=REDIS_PORT,
+                            db=REDIS_DB, password=REDIS_PASSWORD)
